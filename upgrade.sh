cbdf43551cf6457f6705c38c1bcc0e18bca01cc4
bobcui <11597991@qq.com>
2014-12-31 16:56:09 +0800

    add upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm up
+
+rm -rf ./node_modules/pomelo/node_modules/pomelo-admin/node_modules/pomelo-logger
+rm -rf ./node_modules/pomelo/node_modules/pomelo-logger
+rm -rf ./node_modules/pomelo/node_modules/pomelo-rpc/node_modules/pomelo-logger
+rm -rf ./node_modules/pomelo-rpc/node_modules/pomelo-logger
+
+rm -rf ./node_modules/pomelo/node_modules/pomelo-rpc
